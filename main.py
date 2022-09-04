827d6cd58f5807d5eae074b254c81a1d13ae51fe
rishavkumar7 <rishavkumarofficial7@gmail.com>
2022-09-05 01:23:35 +0530

    updated the display statement

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app=FastAPI()
 
 @app.get("/HomePage")
 async def HomePage():
-    return "Welcome all, This is my Assignment 1. Here I've build a basic website using fastAPI."
+    return "Welcome All, This is my Assignment 1. Here I've build a basic website using fastAPI."
